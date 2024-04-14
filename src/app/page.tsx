ec7248fc66a475094b32d6aec18d0eecdb3335b9
k_95.21 <kohta0421kochi@gmail.com>
2024-04-14 22:48:48 +0900

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/organisms/Header';
+
 export default function Home() {
   return (
     <div className="w-full text-center p-3 mx-auto my-0">
-      <h1 className="text-3xl text-red-600">Top Site</h1>
+      <Header />
     </div>
   );
 }
