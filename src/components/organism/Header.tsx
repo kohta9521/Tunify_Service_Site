3e501611983dc242789b08ce4eba8aa06eef83f8
k_95.21 <kohta0421kochi@gmail.com>
2024-07-12 17:21:49 +0900

    add new page about apge

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -16,7 +16,11 @@ const Header = () => {
       </div>
       <div className={styles.rightBox}>
         <ListBox id={1}>
-          <ListItem id="1" link="/login" text="ログイン" />
+          <ListItem id="1" link="/" text="Home" />
+          <ListItem id="2" link="/about" text="About" />
+          <ListItem id="3" link="/service" text="Service" />
+          <ListItem id="4" link="/price" text="Price" />
+          <ListItem id="5" link="/contact" text="Contact" />
         </ListBox>
       </div>
     </div>
