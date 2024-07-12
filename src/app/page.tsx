b34511962bf8ae6e2c7916c99cdea521fe35570f
k_95.21 <kohta0421kochi@gmail.com>
2024-07-12 18:27:32 +0900

    add new header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 // scss
 import styles from "../styles/page.module.scss";
 import Header from "@/components/organism/Header";
+import Hero from "@/components/organism/Hero";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header />
+      <Hero />
     </main>
   );
 }
--- src/components/organism/Hero.tsx
+++ src/components/organism/Hero.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 // scss
 import styles from "./styles/Hero.module.scss";
+import Header from "./Header";
 
 // components
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <Header />
+      </div>
     </div>
   );
 };
