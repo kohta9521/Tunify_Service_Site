5d1c674e2cd822e79073ffc95d850a5385a140ed
k_95.21 <kohta0421kochi@gmail.com>
2024-07-17 14:51:23 +0900

    storybook nextjs scss module and global scss setting

---
+++ src/app/diagnosis/complete/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// scss
+import styles from "./Complete.module.scss";
+
+// components
+
+const Complete = () => {
+  return (
+    <div className={styles.complete}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+
+export default Complete;
--- src/components/organism/Hero.tsx
+++ src/components/organism/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
           width={1920}
           height={1080}
         />
-        <Link href="/">
+        <Link href="/diagnosis">
           <Image
             className={styles.circleImage}
             src="/images/top/circle.png"
