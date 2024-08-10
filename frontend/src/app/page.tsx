8e67bf4546d43309d892e6f7173ec730b0aba18e
k_95.21 <kohta0421kochi@gmail.com>
2024-08-10 18:17:33 +0900

    add new listbox item

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,13 +5,20 @@ import styles from "../styles/page.module.scss";
 // components
 import Header from "@/components/organisms/Header";
 import HeaderList from "@/components/molecules/HeaderList";
+import ListItem from "@/components/atoms/ListItem";
+import ListBoxItem from "@/components/atoms/ListBoxItem";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header id="home">
         <HeaderList id="home">
-          <p>sample</p>
+          <ListItem id="headerItem1" link="/" text="Home" color="white" />
+          <ListItem id="headerItem1" link="/" text="About" color="white" />
+          <ListItem id="headerItem1" link="/" text="service" color="white" />
+          <ListItem id="headerItem1" link="/" text="Company" color="white" />
+          <ListBoxItem id="headerItem1" link="/" text="Login" color="pink" />
+          <ListBoxItem id="headerItem1" link="/" text="Login" color="pink" />
         </HeaderList>
       </Header>
     </main>
