e93b4b2ca580905b2c45e7fd35e1f985aba7299d
k_95.21 <kohta0421kochi@gmail.com>
2024-08-10 18:45:56 +0900

    add new peojects

---
+++ frontend/src/components/organisms/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// scss
+import styles from "./styles/Hero.module.scss";
+
+// components
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+
+export default Hero;
