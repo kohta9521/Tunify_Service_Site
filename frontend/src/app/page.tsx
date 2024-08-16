9e239581f1a25ef7fe505986cec053639cfca7f9
k_95.21 <kohta0421kochi@gmail.com>
2024-08-16 16:44:14 +0900

    add new about template componente

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,6 +9,7 @@ import ListItem from "@/components/atoms/ListItem";
 import HamburgerBtn from "@/components/atoms/HamburgerBtn";
 import Hero from "@/components/organisms/Hero";
 import About from "@/components/organisms/About";
+import AboutHalfScreen from "@/components/templates/AboutHalfScreen";
 
 export default function Home() {
   return (
@@ -24,6 +25,15 @@ export default function Home() {
       </Header>
       <Hero />
       <About />
+      <AboutHalfScreen
+        id="screen1"
+        link="/"
+        img="/images/hero2.jpg"
+        date="2021.10.10"
+        newsName="Sample"
+        outlineText="samplesamplesamplesample"
+        descText="sample samplesamplesamplejdasojasodfosdj"
+      />
     </main>
   );
 }
