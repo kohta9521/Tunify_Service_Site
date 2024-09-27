d018270ceae1c25d2e78bb260d4d9fa0acf01c95
k_95.21 <kohta0421kochi@gmail.com>
2024-09-27 17:56:29 +0900

    add reset this project #28

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,59 +1,9 @@
-// scss
-
-import styles from "../styles/page.module.scss";
-
 // components
-import Header from "@/components/organisms/Header";
-import HeaderList from "@/components/molecules/HeaderList";
-import ListItem from "@/components/atoms/ListItem";
-import HamburgerBtn from "@/components/atoms/HamburgerBtn";
-import Hero from "@/components/organisms/Hero";
-import About from "@/components/organisms/About";
-import AboutHalfScreen from "@/components/templates/AboutHalfScreen";
-import Service from "@/components/organisms/Service";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header id="home">
-        <HeaderList id="home">
-          <ListItem id="headerItem1" link="/" text="Home" color="white" />
-          <ListItem id="headerItem1" link="/" text="About" color="white" />
-          <ListItem id="headerItem1" link="/" text="service" color="white" />
-          <ListItem id="headerItem1" link="/" text="Company" color="white" />
-          <HamburgerBtn />
-        </HeaderList>
-      </Header>
-      <Hero />
-      {/* <About />
-      <AboutHalfScreen
-        id="screen1"
-        link="/"
-        img="/images/hero2.jpg"
-        date="2021.10.10"
-        newsName="Sample"
-        outlineText="samplesamplesamplesample"
-        descText="sample samplesamplesamplejdasojasodfosdj"
-      />
-      <AboutHalfScreen
-        id="screen1"
-        link="/"
-        img="/images/hero1.jpg"
-        date="2021.10.10"
-        newsName="Sample"
-        outlineText="samplesamplesamplesample"
-        descText="sample samplesamplesamplejdasojasodfosdj"
-      />
-      <AboutHalfScreen
-        id="screen1"
-        link="/"
-        img="/images/hero.jpeg"
-        date="2021.10.10"
-        newsName="Sample"
-        outlineText="samplesamplesamplesample"
-        descText="sample samplesamplesamplejdasojasodfosdj"
-      />
-      <Service /> */}
+    <main>
+      <h1>hello world</h1>
     </main>
   );
 }
